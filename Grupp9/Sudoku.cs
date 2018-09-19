745f03c2ade5599f242417f86df6d74a8f185399
Sebastian Sagrelius <sagrelius@live.se>
2018-09-19 15:58:50 +0200

    Det funkar!

--- Grupp9/Sudoku.cs
+++ Grupp9/Sudoku.cs
@@ -9,51 +9,52 @@ namespace Grupp9
     public class Sudoku
     {
 
-        private int[,] gameNumbersArray;
+        private int[,] board = new int[9, 9];
 
-        public Sudoku (string gameNumbers)
+        public Sudoku(string board)
         {
-            gameNumbersArray = NumbersToBoard(gameNumbers);
+            // TODO: Put string in board
+            //  Gå igenom varje tecken i strängen.
+
+            for (int i = 0; i < 9 * 9; i++)
+            {
+                //   Gör om tecken till siffra.
+                int number = int.Parse(board[i].ToString());
+                //    Räkna ut rad och kolumn.
+                int row = i / 9;
+                int col = i % 9;
+                //     Skriva in siffra i cell.
+                SetCellValue(row, col, number);
+            }
+            
         }
 
-        private int[,] NumbersToBoard(string gameNumbers)
+        private int GetCellValue(int row, int col)
         {
-            char[] gameNumbersToCharArray = gameNumbers.ToCharArray();
-            int[,] gameNumbersToIntArray = new int[9, 9];
+            return board[row, col];
 
-            int row = 0;
-            int col = 0;
+        }
 
-            foreach (char character in gameNumbersToCharArray)
-            {
-                int numbers = int.Parse(character.ToString());
-                gameNumbersToIntArray[row, col] = numbers;
+        private void SetCellValue(int row, int col, int value)
+        {
+            board[row, col] = value;
 
-                col++;
-                if (col == 9)
-                {
-                    col = 0;
-                    row ++;
-                }
-
-            }
-            return gameNumbersToIntArray;
         }
 
 
         public void PrintBoard()
         {
             PrintBorder();
-            for (int row = 0; row < gameNumbersArray.GetLength(0); row++)
+            for (int row = 0; row < board.GetLength(0); row++)
             {
                 Console.Write("|");
-                for (int col = 0; col < gameNumbersArray.GetLength(1); col++)
+                for (int col = 0; col < board.GetLength(1); col++)
                 {
                     if (col == 3 || col == 6)
                     {
                         Console.Write("|");
                     }
-                    Console.Write(gameNumbersArray[row, col]);
+                    Console.Write(board[row, col]);
                 }
                 Console.WriteLine("|");
 
@@ -69,55 +70,117 @@ namespace Grupp9
         public void PrintBorder()
         {
             Console.Write("+");
-            for (int col = 0; col-1 < gameNumbersArray.GetLength(1)+1; col++)
+            for (int col = 0; col-1 < board.GetLength(1)+1; col++)
             {
                 Console.Write("-");
             }
             Console.WriteLine("+");
         }
 
-        public void Solve()
+        
+
+        private int[] GetNumbersInRow(int row)
         {
-            int row = 0;
-            
-            do
+            // Hämta siffrorna i en rad.
+            int[] result = new int[9];
+
+            int position = 0;
+
+            for (int col = 0; col < 9; col++)
             {
-                for (int col = 0; col < gameNumbersArray.GetLength(1); col++)
+                int cellValue = GetCellValue(row, col);
+                result[position] = cellValue;
+                position++;
+            }
+
+            return result;
+            
+        }
+
+        private int[] GetNumbersInColumn(int col)
+        {
+            int[] result = new int[9];
+
+            int position = 0;
+
+            for (int row = 0; row < 9; row++)
+            {
+                int cellValue = GetCellValue(row, col);
+                result[position] = cellValue;
+                position++;
+            }
+
+            return result;
+            
+        }
+
+
+        private int[] GetNumbersInBlock(int row, int col)
+        {
+            // Beräkna vilket block
+            //  Hämta siffrorna i blocket rad för rad
+
+            int[] result = new int[9];
+            int topLeftRow = (row / 3) * 3;
+            int topLeftCol = (col / 3) * 3;
+            int position = 0;
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
                 {
-                    if (gameNumbersArray[row,col] == 0)
-                    {
-                        List<int> possibleNumbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-
-                        for (gameNumbersArray.GetValue(row,col); col < gameNumbersArray.GetLength(1); col++)
-                        {
-                            if (gameNumbersArray[row, col] != 0)
-                            {
-                               possibleNumbers.Remove(gameNumbersArray[row, col]);
-
-                                if (possibleNumbers.Contains(1))
-                                {
-                                    gameNumbersArray[row, col] = possibleNumbers[0];
-                                }
-                            }
-                            if (col == 9)
-                            {
-                               break;
-                            }
-                        }
-                        row++;
-                    }
+                    int cellValue = GetCellValue(topLeftRow + i, topLeftCol + j);
+                    result[position] = cellValue;
+                    position++;
                 }
-                
-            } while (WhileNoEmptyCell());
+            }
+
+            return result;
+        }
+
+        public int[] FindPossibleNumbers(int row, int col)
+        {
+
+            // Hitta möjliga tal för cell utifrån rad, kolumn och block.
+            List<int> result = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            int[] numbersInRow = GetNumbersInRow(row);
+            int[] numbersInColumn = GetNumbersInColumn(col);
+            // int[] numbersInBlock = GetNumbersInBlock(row, col);
+
+            foreach (var number in numbersInRow)
+            {
+                if (result.Contains(number))
+                {
+                    result.Remove(number);
+                }
+            }
+            foreach (var number in numbersInColumn)
+            {
+                if (result.Contains(number))
+                {
+                    result.Remove(number);
+                }
+            }
+            foreach (var number in GetNumbersInBlock(row, col))
+            {
+                if (result.Contains(number))
+                {
+                    result.Remove(number);
+                }
+            }
+
+
+            return result.ToArray();
+
         }
 
         private bool WhileNoEmptyCell()
         {
-            for (int row = 0; row < gameNumbersArray.GetLength(0); row++)
+            for (int row = 0; row < board.GetLength(0); row++)
             {
-                for (int col = 0; col < gameNumbersArray.GetLength(1); col++)
+                for (int col = 0; col < board.GetLength(1); col++)
                 {
-                    if (gameNumbersArray[row, col] == 0)
+                    if (board[row, col] == 0)
                     {
                         return true;
                     }
@@ -126,59 +189,41 @@ namespace Grupp9
             return false;
         }
 
+        public void Solve()
+        {
+            Console.WriteLine("Tryck enter för att lösa. ");
+            Console.ReadLine();
 
+            do
+            {
+                for (int row = 0; row < 9; row++)
+                {
+                    for (int col = 0; col < 9; col++)
+                    {
+                        if (board[row, col] == 0)
+                        {
+                            FindPossibleNumbers(row, col);
 
+                            if (FindPossibleNumbers(row, col).Length == 0)
+                            {
+                                break;
+                            }
+                            else if (FindPossibleNumbers(row,col).Length == 1)
+                            {
+                                board[row, col] = FindPossibleNumbers(row, col)[0];
+                            }
+                            else
+                            {
+                                continue;
+                            }
+                        }
+                    }
+                }
+            } while (WhileNoEmptyCell());
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        
-
-
-
-
-
+            PrintBoard();
+        }
 
     }
-
-	
-        
-        
-
-
-        
-    
+ 
 }
