8d7c5b4c71ddcc9598fc50f756a58725aad7bcc4
Sebastian Sagrelius <sagrelius@live.se>
2018-10-01 12:53:36 +0200

    ändrade skip

--- AdventureGameTEst/AdventureGameTEst/Classes/GameBuilder.cs
+++ AdventureGameTEst/AdventureGameTEst/Classes/GameBuilder.cs
@@ -203,7 +203,7 @@ namespace AdventureGameTEst.Classes
                         }
                         else
                         {
-                            input = input.Skip(0).ToArray();
+                            input = input.Skip(1).ToArray();
                         }
 
                         foreach (Exit exit in exits)
